64f2b2793cf6d1b5014c6d4c662c2b97c024fe21
kaverynfobba05-dot <kaverynfobba05@gmail.com>
2026-03-28 23:12:36 +0530

    Delete student_result.py

--- student_result.py
+++
@@ -1,9 +0,0 @@
-marks=int(input("Enter student's marks:"))
-if (marks>=35):
-    print("Pass")
-    if (marks>=75):
-        print("Distinction")  
-    elif (marks>=60):
-        print("First class")
-else:
-    print("Fail")
